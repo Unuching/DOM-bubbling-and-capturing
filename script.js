c4db44a6a96d026568eae71fabb39a098a84e08f
Unuching marma <unuching.marma2023@gmail.com>
2024-09-01 21:40:00 +0600

    ja dom bubbling and capturing

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("um1").addEventListener(
+  "click",
+  function () {
+    alert("White");
+  },
+  false
+);
+document.getElementById("div1").addEventListener(
+  "click",
+  function () {
+    alert("Green");
+  },
+  false
+);
+document.getElementById("um2").addEventListener(
+  "click",
+  function () {
+    alert("White");
+  },
+  true
+);
+document.getElementById("div2").addEventListener(
+  "click",
+  function () {
+    alert("Green");
+  },
+  true
+);
